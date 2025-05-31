33e7a046d11d869c6079ab79122299bd09f12c52
PDL Institute <mail2pdlinstitute@gmail.com>
2025-05-31 15:22:12 +0530

    new site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,10 @@
-import type { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
   siteUrl: 'https://www.webdeveloperdeepak.com', // Replace with your domain
   generateRobotsTxt: true,
   exclude: ['/secret', '/admin/*'],
   robotsTxtOptions: {
     policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
+      { userAgent: '*', allow: '/' }],
   },
 };
-
-export default config;
